f5d58a3d3a4620c7522ee42c3bbaf8d0b8729f1a
binwan-dev <email@wanbin.tech>
2024-04-26 11:51:31 +0800

    feat: add Snowflake

---
+++ src/BinTool/Extensions/SnowflakeExtension.cs
@@ -0,0 +1,18 @@
+using Snowflake.Core;
+
+namespace System;
+
+public static class SnowflakeExtension
+{
+    private static IdWorker Worker = new IdWorker(1, 1);
+
+    public static void SetWorker(int workerId, int dataCenterId, int sequence = 0)
+    {
+        Worker = new IdWorker(workerId, dataCenterId, sequence);
+    }
+
+    public static long NewId()
+    {
+        rn Worker.NextId();
+    }
+}
